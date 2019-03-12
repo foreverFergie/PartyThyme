7eac6b838f21f030e0d1827fe893187da628daf9
Creem <creemb@wit.edu>
2019-03-11 22:21:19 -0400

    Added stuff for getting data from database

--- app/src/main/java/edu/wit/mobileapp/partythyme/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/partythyme/MainActivity.java
@@ -1,8 +1,13 @@
 package edu.wit.mobileapp.partythyme;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +19,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar =(Toolbar)findViewById(R.id.toolbar);
+
+
+
         myDb = new DatabaseHelper(this);
+        String[] columns = {"name", "annual", "biennial"};
+        String where = "name = ?";
+        String[] where_args = {"Basil"};
+        String group_by = null;
+        String having = null;
+        String order_by = "name";
+        Cursor cursor = myDb.db.query("plants", columns, where, where_args, group_by, having, order_by);
+        while(cursor.moveToNext()){
+              String name = cursor.getString(cursor.getColumnIndex("NAME"));
+              int annual = cursor.getInt(cursor.getColumnIndex("ANNUAL")) ;
+              int biennial = cursor.getInt(cursor.getColumnIndex("BIENNIAL"));
+              Log.v("myApp", "name: " + name + ", annual: " + annual + ", biennial: " + biennial);
+              System.out.println(name);
+        }
     }
 }
