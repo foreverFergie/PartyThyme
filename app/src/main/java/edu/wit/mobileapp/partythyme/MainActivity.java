f35b93dafee7ecf259873a74a830d3f3b1f2996b
Creem <creemb@wit.edu>
2019-03-12 18:15:05 -0400

    Finished primary database work. More work needed when searching when trying to add a plant

--- app/src/main/java/edu/wit/mobileapp/partythyme/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/partythyme/MainActivity.java
@@ -19,23 +19,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar =(Toolbar)findViewById(R.id.toolbar);
-
-
-
-        myDb = new DatabaseHelper(this);
-        String[] columns = {"name", "annual", "biennial"};
-        String where = "name = ?";
-        String[] where_args = {"Basil"};
-        String group_by = null;
-        String having = null;
-        String order_by = "name";
-        Cursor cursor = myDb.db.query("plants", columns, where, where_args, group_by, having, order_by);
-        while(cursor.moveToNext()){
-              String name = cursor.getString(cursor.getColumnIndex("NAME"));
-              int annual = cursor.getInt(cursor.getColumnIndex("ANNUAL")) ;
-              int biennial = cursor.getInt(cursor.getColumnIndex("BIENNIAL"));
-              Log.v("myApp", "name: " + name + ", annual: " + annual + ", biennial: " + biennial);
-              System.out.println(name);
-        }
     }
 }
