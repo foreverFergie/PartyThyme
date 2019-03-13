8775d39355fc81f9a2088f8514dc7173d7f6ad45
Creem <creemb@wit.edu>
2019-03-13 18:44:42 -0400

    Added rough draft for plant_info

--- app/src/main/java/edu/wit/mobileapp/partythyme/plant_info.java
+++ app/src/main/java/edu/wit/mobileapp/partythyme/plant_info.java
@@ -1,11 +1,22 @@
 package edu.wit.mobileapp.partythyme;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
 
 public class plant_info extends AppCompatActivity {
 
@@ -13,17 +24,53 @@ public class plant_info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_info);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Bundle bundle = this.getIntent().getExtras();
+        //Get what plant this activity will be used for
+        String name = bundle.getString("name");
+        Plant plant = new Plant(name, this);
+
+        //Update image based on plant
+        ImageView plantImage = (ImageView) findViewById(R.id.plantImage);
+        Bitmap plantBitmap = BitmapFactory.decodeResource(getResources(), getResources().getIdentifier(plant + "/" + name + ".png", "mipmap", this.getPackageName()));
+        plantImage.setImageBitmap(plantBitmap);
+
+        //Update displayed plant name based on plant
+        TextView dispName = (TextView) findViewById(R.id.plantName);
+        dispName.setText(name);
+
+        //List all plant facts in database
+        ListView plantFacts =(ListView) findViewById(R.id.factList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+
+        adapter.add("Annual: " + boolToYesOrNo(plant.annual));
+        adapter.add("Biennial: " + boolToYesOrNo(plant.biennial));
+        adapter.add("Perennial: " + boolToYesOrNo(plant.perrenial));
+        adapter.add("Indoor: " + boolToYesOrNo(plant.indoor));
+        adapter.add("Maintenance (1-3): " + plant.maintenance);
+        adapter.add("Water Maintenance: " + plant.waterMaintenance);
+        adapter.add("Soil: " + plant.soil);
+        adapter.add("Fertilizer: " + plant.fertilizer);
+        adapter.add("Sun Level Summer: " + plant.sunMaintainSummer);
+        adapter.add("Sun Level Winter: " + plant.sunMaintainWinter);
+        adapter.add("PH Level: " + plant.PHMaintain);
+        adapter.add("Pruning: " + plant.pruning);
+        adapter.add("Flowering: " + boolToYesOrNo(plant.flowering));
+        adapter.add("Poisonous: " + boolToYesOrNo(plant.poison));
+        adapter.add("Tubular: " + boolToYesOrNo(plant.tubular));
+        adapter.add("Cactus: " + boolToYesOrNo(plant.cactus));
+        adapter.add("Tree: " + boolToYesOrNo(plant.tree));
+        adapter.add("Can be used for landscaping: " + boolToYesOrNo(plant.landscaping));
+        adapter.add("USDA Hardiness Zone: " + plant.hardinessZone);
+        plantFacts.setAdapter(adapter);
     }
+
+    private String boolToYesOrNo(boolean b){
+        if(b){
+            return "Yes";
+        }
+        return "No";
+    }
+
 
 }
