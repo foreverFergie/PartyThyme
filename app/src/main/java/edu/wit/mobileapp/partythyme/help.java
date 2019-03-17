a53dc2c7e80ac2a727a3f46a22c97d3de293a908
Creem <creemb@wit.edu>
2019-03-17 13:55:19 -0400

    Added super basic help screen. No way to access it yet but its there

--- app/src/main/java/edu/wit/mobileapp/partythyme/help.java
+++ app/src/main/java/edu/wit/mobileapp/partythyme/help.java
@@ -1,11 +1,13 @@
 package edu.wit.mobileapp.partythyme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class help extends AppCompatActivity {
 
@@ -13,17 +15,17 @@ public class help extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button home = (Button) findViewById(R.id.homeButton);
+        home.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(help.this, MainActivity.class);
+                startActivity(intent);
             }
         });
+
     }
 
 }
