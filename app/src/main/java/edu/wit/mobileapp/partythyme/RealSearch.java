3c0ee610a7abdfac61ff64c04dcb616fcb9142bf
Creem <creemb@wit.edu>
2019-03-18 21:29:59 -0400

    Added images to search result list

--- app/src/main/java/edu/wit/mobileapp/partythyme/RealSearch.java
+++ app/src/main/java/edu/wit/mobileapp/partythyme/RealSearch.java
@@ -3,6 +3,8 @@ package edu.wit.mobileapp.partythyme;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +14,9 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RealSearch extends AppCompatActivity {
 
@@ -41,18 +46,32 @@ public class RealSearch extends AppCompatActivity {
                 String order_by = "name";
                 Cursor cursor = myDb.db.query("plants", columns, where, where_args, group_by, having, order_by);
 
-                final ArrayAdapter<String> result = new ArrayAdapter<String>(RealSearch.this, android.R.layout.simple_list_item_1);
+                Bitmap defaultImage = BitmapFactory.decodeResource(getResources(), R.drawable.default_plant);
+                List<ListItem> list = new ArrayList<ListItem>();
+
 
                 //Add plants by name
                 while(cursor.moveToNext()) {
+                    ListItem myItem = new ListItem();
                     Plant p = new Plant(cursor.getString(cursor.getColumnIndex("NAME")), RealSearch.this);
-                    result.add(p.name);
+                    myItem.name = p.name;
+                    int plantImageNum = getPlantImage(RealSearch.this, p.name);
+                    if(plantImageNum > 0) {
+                        myItem.image = BitmapFactory.decodeResource(getResources(), plantImageNum);
+                    }
+                    else
+                    {
+                        myItem.image = BitmapFactory.decodeResource(getResources(), R.drawable.default_plant);
+                    }
+                    list.add(myItem);
                     Log.v("myApp", p.name);
                 }
 
+                final ListItemAdapter adapter;
+                adapter = new ListItemAdapter(RealSearch.this, 0, list);
 
                 ListView plantList = (ListView) findViewById(R.id.search_list_result);
-                plantList.setAdapter(result);
+                plantList.setAdapter(adapter);
 
                 plantList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
@@ -60,7 +79,7 @@ public class RealSearch extends AppCompatActivity {
                         Intent intent = new Intent(RealSearch.this, plant_info_add.class);
                         Bundle bundle = new Bundle();
                         try {
-                            bundle.putString("name", result.getItem(position));
+                            bundle.putString("name", adapter.getItem(position).name);
                             intent.putExtras(bundle);
                             startActivity(intent);
                         }catch(java.lang.NullPointerException e){
@@ -71,5 +90,9 @@ public class RealSearch extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    public static int getPlantImage(Context context, String name){
+        return context.getResources().getIdentifier(name.toLowerCase(), "drawable", context.getPackageName());
     }
 }
