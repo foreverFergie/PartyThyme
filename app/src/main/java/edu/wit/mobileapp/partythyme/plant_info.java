4ce3e756383c60d2891930d5e5460991631a650d
Erinn <erinnf44@gmail.com>
2019-04-12 08:54:28 -0400

    actually added all toolbars

--- app/src/main/java/edu/wit/mobileapp/partythyme/plant_info.java
+++ app/src/main/java/edu/wit/mobileapp/partythyme/plant_info.java
@@ -11,6 +11,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -30,6 +32,10 @@ public class plant_info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_info);
+
+        Toolbar toolbar = findViewById(R.id.plantViewToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         Bundle bundle = this.getIntent().getExtras();
         //Get what plant this activity will be used for
@@ -104,5 +110,39 @@ public class plant_info extends AppCompatActivity {
         return "No";
     }
 
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_bar,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.action_help:
+                Intent settingsPage =  new Intent();
+                settingsPage.setClass(plant_info.this,help.class);
+                startActivity(settingsPage);
+                return true;
+
+            case R.id.action_settings:
+                Intent settings = new Intent();
+                settings.setClass(plant_info.this,settings.class);
+                startActivity(settings);
+                return true;
+            case R.id.action_home:
+                Intent homePage = new Intent();
+                homePage.setClass(plant_info.this,MainActivity.class);
+                startActivity(homePage);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
 
 }
